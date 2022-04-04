50440969c6d801a10348abce0859d4409e216afc
kimmingyu <soil0201@gmail.com>
2022-04-04 17:21:51 +0900

    ADD: Section1, Section2
    mkdir Section2
    add file Memo.md
    add project core

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    // MemberServiceImpl 은 MemberRepository(인터페이스=추상화), MemoryMemberRepository(구현체)에도 의존한다.
+    // DIP 위반.
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
